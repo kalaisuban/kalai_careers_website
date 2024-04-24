4fe3d776d8d3ab7f5048081f6fa817ec352c36fd
kalaiselvan M P S <135983593+kalaisuban@users.noreply.github.com>
2024-04-24 16:51:24 +0000

    rendered sample web page

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
-
+from flask import Flask,render_template
+from sample_answer import Sample
 app=Flask(__name__)
 
+sam=Sample()
 @app.route("/")
 def hello_world():
-  return "Hello,This is kalaiking45"
+  # return "Hello,This is kalaiking45"
+  return render_template("home.html",temp=sam.get_output())
 
 if __name__=="__main__":
-  app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0',debug=True) 
---
+++ sample_answer.py
@@ -0,0 +1,7 @@
+class Sample:
+  def __inti__(self):
+    pass
+
+  def get_output(self):
+    print("Kalai")
+    return "Kalaiselvan M P S"
