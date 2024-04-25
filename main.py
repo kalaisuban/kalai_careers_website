28812fc2ffe40c45d7119c1c14e447bf9061e110
kalaiselvan M P S <135983593+kalaisuban@users.noreply.github.com>
2024-04-25 16:17:23 +0000

    added data

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask, jsonify,render_template
 from sample_answer import Sample
 app=Flask(__name__)
-
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  }
+]
+# company_name="Kalai"
 sam=Sample()
 @app.route("/")
 def hello_world():
   # return "Hello,This is kalaiking45"
-  return render_template("home.html",temp=sam.get_output())
+  return render_template("home.html",temp=sam.get_output(),temp1=JOBS,company_name="Kalai")
 
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True) 
